e68affcdc1e9fcb79967c738050557674106aa01
garesuta <garesuta@gmail.com>
2021-02-08 21:32:18 +0700

    add detail to MainActivity to response onClick

--- app/src/main/java/com/example/joke/MainActivity.java
+++ app/src/main/java/com/example/joke/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.joke;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void onClick(View view){
+        Intent intent = new Intent(this, DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,getResources().getString(R.string.response));
+        startService(intent);
     }
 }
