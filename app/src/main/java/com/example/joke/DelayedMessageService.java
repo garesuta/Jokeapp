307eb0c64c5c714cfba47eaaa9b025c947e69478
garesuta <garesuta@gmail.com>
2021-02-09 22:11:30 +0700

    Set notification

--- app/src/main/java/com/example/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/joke/DelayedMessageService.java
@@ -2,12 +2,16 @@ package com.example.joke;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.app.PendingIntent;
+import android.app.NotificationManager;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 
 public class DelayedMessageService extends IntentService {
 
     public static final  String EXTRA_MESSAGE = "message";
+    public static  final int NOTIFICATION_ID = 5453;
     public DelayedMessageService() {
         super("DelayedMessageService");
     }
@@ -26,7 +30,23 @@ public class DelayedMessageService extends IntentService {
 
     }
     private void showText(final String text){
-        Log.v("DelayedMessageService","The message is:" + text);
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                .setContentTitle(getString(R.string.question))
+                .setContentText(text)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[] {0,1000})
+                .setAutoCancel(true);
+    Intent actionIntent = new Intent(this,MainActivity.class);
+    PendingIntent actionPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            actionIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+    );
+    builder.setContentIntent(actionPendingIntent);
+    NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+    notificationManager.notify(NOTIFICATION_ID,builder.build());
     }
-
 }
